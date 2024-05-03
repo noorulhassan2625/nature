8cd5c2aa1e52f62a1b9cc6c6d18e92dccab445c2
Mirza Noor <110339296+noorulhassan2625@users.noreply.github.com>
2024-05-03 15:52:28 +0500

    Add files via upload

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Card({ title, imageUrl, description, link }) {
   const location = useLocation();
@@ -10,13 +10,19 @@ function Card({ title, imageUrl, description, link }) {
   return (
     <>
       <div className="card">
-        <div className='card-info'>
-        <img src={imageUrl} width="300px" height="200px" alt={title} />
-        <div className="container">
-          <h4 className='mt-2'><b>{title}</b></h4>
-          <p className='mb-4'>{description}</p>
-          {isHomePage && <Link className='border-b-2 border-[#6BB4B4] text-white bg-[#075c5c]  px-2 py-1' to={link}>Explore More</Link>}
-        </div>
+        <div className="card-info">
+          <img src={imageUrl} width="300px" height="200px" alt={title} />
+          <div className="container">
+            {/* <p className='mb-4'>{description}</p> */}
+            {isHomePage && (
+              <Link
+                className="border-b-2 border-[#6BB4B4] text-white px-2 py-1"
+                to={link}
+              >
+                <h4 className="mt-2 card-tile">{title} <span className="arrow">&rarr;</span></h4>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </>
